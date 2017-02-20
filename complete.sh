772c593e24ac4defc834f12b6f8a8965ddc7f2a3
Andy Edwards <jedwards@fastmail.com>
2017-02-20 10:31:34 -0600

    fix(complete.sh): fix path error

--- complete.sh
+++ complete.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-cd {{ name }}
 git remote rename origin skeleton
 ./merge-json.js package.json PROJECT-package.json > package.json
 rm merge-json.js PROJECT-package.json complete.sh
