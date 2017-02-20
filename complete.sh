4c630b2b6bd329a45a78d8289edf088ac946e0d8
Andy Edwards <jedwards@fastmail.com>
2017-02-20 10:33:29 -0600

    fix(pollinate): fix missing lodash.merge issue

--- complete.sh
+++ complete.sh
@@ -2,7 +2,9 @@
 set -e
 
 git remote rename origin skeleton
+npm i lodash.merge
 ./merge-json.js package.json PROJECT-package.json > package.json
+npm rm lodash.merge
 rm merge-json.js PROJECT-package.json complete.sh
 git add --all .
 git commit -n -m 'pollinate project'"
