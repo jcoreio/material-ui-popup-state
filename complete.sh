f1a3ee9a7bda07419aeba092bbdb906ade7f7b79
Andy Edwards <jedwards@fastmail.com>
2017-03-06 14:00:04 -0600

    refactor(template.json): use new pollinate features

--- complete.sh
+++ complete.sh
@@ -2,10 +2,9 @@
 set -e
 
 git remote rename origin skeleton
-npm i lodash.merge
-./merge-package-json.js
-npm rm lodash.merge
-rm merge-package-json.js PROJECT-package.json complete.sh
+git branch -D master
+git checkout -b master
+rm complete.sh
 git add --all .
 git commit -n -m 'pollinate project'
 
