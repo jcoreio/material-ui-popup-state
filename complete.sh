33c27d2ed6823e84c8c8ac9e80116e5e35bb8152
Andy Edwards <jedwards@fastmail.com>
2017-06-10 00:07:27 -0500

    fix(complete.sh): fix bugs with master branch and upstream
    complete.sh would fail if pollinating master branch.  Also, it would leave skeleton as the upstream
    
    of the new master branch, leading me to accidentally push changes from new projects to the skeleton
    
    repo.

--- complete.sh
+++ complete.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+if [[ $(git symbolic-ref --short HEAD) != 'master' ]]; then
+  git branch -D master
+  git checkout -b master
+fi
+git branch --unset-upstream master
 git remote rename origin skeleton
+git remote add origin $(node -p 'require("./package").repository.url')
 rm complete.sh
 git add --all .
 git commit -n -m 'pollinate project'
