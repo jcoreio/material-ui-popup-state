b0ca5862f48a9751b4820eae7bc5c90dcb60ebe4
Andy Edwards <jedwards@fastmail.com>
2019-02-15 01:53:39 -0600

    docs(TogglePopover): use hooks API instead

--- demo/examples/TogglePopover.js
+++ demo/examples/TogglePopover.js
@@ -4,7 +4,11 @@ import { withStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Popover from '@material-ui/core/Popover'
-import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state'
+import {
+  usePopupState,
+  bindTrigger,
+  bindPopover,
+} from 'material-ui-popup-state/hooks'
 
 const styles = theme => ({
   typography: {
@@ -12,32 +16,34 @@ const styles = theme => ({
   },
 })
 
-const PopoverPopupState = ({ classes }) => (
-  <PopupState variant="popover" popupId="demoPopover">
-    {popupState => (
-      <div>
-        <Button variant="contained" {...bindTrigger(popupState)}>
-          Open Popover
-        </Button>
-        <Popover
-          {...bindPopover(popupState)}
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'center',
-          }}
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'center',
-          }}
-        >
-          <Typography className={classes.typography}>
-            The content of the Popover.
-          </Typography>
-        </Popover>
-      </div>
-    )}
-  </PopupState>
-)
+const PopoverPopupState = ({ classes }) => {
+  const popupState = usePopupState({
+    variant: 'popover',
+    popupId: 'demoPopover',
+  })
+  return (
+    <div>
+      <Button variant="contained" {...bindTrigger(popupState)}>
+        Open Popover
+      </Button>
+      <Popover
+        {...bindPopover(popupState)}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <Typography className={classes.typography}>
+          The content of the Popover.
+        </Typography>
+      </Popover>
+    </div>
+  )
+}
 
 PopoverPopupState.propTypes = {
   classes: PropTypes.object.isRequired,
