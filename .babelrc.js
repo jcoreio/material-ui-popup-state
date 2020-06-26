162bc053771dffc47f47cd50367ee7231ccd71d2
Andy Edwards <jedwards@fastmail.com>
2020-06-26 13:15:08 -0500

    fix(hoverWorkaround): use inline styles only

--- .babelrc.js
+++ .babelrc.js
@@ -6,6 +6,7 @@ module.exports = function(api) {
     '@babel/plugin-proposal-export-namespace-from',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-optional-chaining',
   ]
   const presets = [
     [
--- src/hoverWorkaround.js
+++ src/hoverWorkaround.js
@@ -1,37 +1,17 @@
 import * as React from 'react'
-import withStyles from '@material-ui/core/styles/withStyles'
 import classNames from 'classnames'
-
-const styles = {
-  _modalRoot: {
-    pointerEvents: 'none',
-  },
-  paper: {
-    pointerEvents: 'auto',
-  },
-}
 
 export default function hoverWorkaround(Comp) {
   /* eslint-disable react/display-name */
-  const HoverWorkaround = React.forwardRef(
-    (
-      {
-        classes: { _modalRoot, ...classes },
-        ModalClasses,
-        style,
-        className,
-        ...props
-      },
-      ref
-    ) => (
-      <Comp
-        ref={ref}
-        classes={classes}
-        className={classNames(className, _modalRoot)}
-        style={{ pointerEvents: 'none', ...style }}
-        {...props}
-      />
-    )
-  )
-  return withStyles(styles)(HoverWorkaround)
+  return React.forwardRef(({ PaperProps, style, ...props }, ref) => (
+    <Comp
+      ref={ref}
+      style={{ pointerEvents: 'none', ...style }}
+      PaperProps={{
+        style: { pointerEvents: 'auto', ...PaperProps?.style },
+        ...PaperProps,
+      }}
+      {...props}
+    />
+  ))
 }
