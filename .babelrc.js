9ec4fec2fe241abf0929b6881a426cd27e02bd80
Andy Edwards <jedwards@fastmail.com>
2021-01-27 15:09:55 -0600

    chore: use node 14, upgrade deps and config

--- .babelrc.js
+++ .babelrc.js
@@ -1,18 +1,14 @@
 module.exports = function (api) {
   const plugins = [
     '@babel/plugin-transform-flow-strip-types',
-    '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-export-default-from',
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-object-rest-spread',
   ]
   const presets = [
     [
       '@babel/preset-env',
       api.env('es5')
         ? { forceAllTransforms: true }
-        : { targets: { node: 'current' } },
+        : { targets: { node: '12' } },
     ],
     '@babel/preset-flow',
   ]
