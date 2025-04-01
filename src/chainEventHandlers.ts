dd380dabff36bbe65b5bf6c4d964926de1d88152
schroda <50052685+schroda@users.noreply.github.com>
2025-04-02 01:05:25 +0200

    fix: improve chainEventHandlers type
    support spreading/passing more than 8 arguments and make the return type more accurate

--- src/chainEventHandlers.ts
+++ src/chainEventHandlers.ts
@@ -1,88 +1,42 @@
-/* eslint-disable @typescript-eslint/ban-types */
-export function chainEventHandlers<T1 extends {}, T2 extends {}>(
-  t1: T1,
-  t2: T2
-): T1 & T2
-export function chainEventHandlers<T1 extends {}, T2 extends {}, T3 extends {}>(
-  t1: T1,
-  t2: T2,
-  t3: T3
-): T1 & T2 & T3
-export function chainEventHandlers<
-  T1 extends {},
-  T2 extends {},
-  T3 extends {},
-  T4 extends {}
->(t1: T1, t2: T2, t3: T3, t4: T4): T1 & T2 & T3 & T4
-export function chainEventHandlers<
-  T1 extends {},
-  T2 extends {},
-  T3 extends {},
-  T4 extends {},
-  T5 extends {}
->(t1: T1, t2: T2, t3: T3, t4: T4, t5: T5): T1 & T2 & T3 & T4 & T5
-export function chainEventHandlers<
-  T1 extends {},
-  T2 extends {},
-  T3 extends {},
-  T4 extends {},
-  T5 extends {},
-  T6 extends {}
->(t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6): T1 & T2 & T3 & T4 & T5 & T6
-export function chainEventHandlers<
-  T1 extends {},
-  T2 extends {},
-  T3 extends {},
-  T4 extends {},
-  T5 extends {},
-  T6 extends {},
-  T7 extends {}
->(
-  t1: T1,
-  t2: T2,
-  t3: T3,
-  t4: T4,
-  t5: T5,
-  t6: T6,
-  t7: T7
-): T1 & T2 & T3 & T4 & T5 & T6 & T7
-export function chainEventHandlers<
-  T1 extends {},
-  T2 extends {},
-  T3 extends {},
-  T4 extends {},
-  T5 extends {},
-  T6 extends {},
-  T7 extends {},
-  T8 extends {}
->(
-  t1: T1,
-  t2: T2,
-  t3: T3,
-  t4: T4,
-  t5: T5,
-  t6: T6,
-  t7: T7,
-  t8: T8
-): T1 & T2 & T3 & T4 & T5 & T6 & T7 & T8
-export function chainEventHandlers(
-  first: Record<string, any>,
-  ...rest: Record<string, any>[]
-): Record<string, any> {
-  const result: Record<string, any> = { ...first }
-  for (const obj of rest) {
-    for (const key in obj) {
-      const value = obj[key]
-      const prev = result[key]
-      if (typeof prev === 'function' && typeof value === 'function') {
-        result[key] = (...args: any[]) => {
-          prev(...args)
-          return value(...args)
+type Assign<A, B> = {
+    [K in keyof A | keyof B]:
+    K extends keyof A
+        ? K extends keyof B
+            ? NonNullable<A[K]> extends (...args: any[]) => any
+                ? NonNullable<B[K]> extends (...args: any[]) => any
+                    ? A[K] | B[K]
+                    : B[K]
+                : B[K]
+            : A[K]
+        : K extends keyof B
+            ? B[K]
+            : never
+}
+
+type MergeObjectsArray<T extends object[]> = T extends [infer F, ...infer R]
+    ? Assign<F, R extends object[] ? MergeObjectsArray<R> : NonNullable<unknown>>
+    : NonNullable<unknown>;
+
+type PrettyObject<T> = { [K in keyof T]: T[K] } & NonNullable<unknown>
+
+export function chainEventHandlers<First extends Record<string, any>, Rest extends Record<string, any>[]>(
+    first: First,
+    ...rest: Rest
+): PrettyObject<MergeObjectsArray<[First, ...Rest]>> {
+    const result: Record<string, any> = {...first}
+    for (const obj of rest) {
+        for (const key in obj) {
+            const value = obj[key]
+            const prev = result[key]
+            if (typeof prev === 'function' && typeof value === 'function') {
+                result[key] = (...args: any[]) => {
+                    prev(...args)
+                    return value(...args)
+                }
+            } else {
+                result[key] = value
+            }
         }
-      } else {
-        result[key] = value
-      }
     }
-  }
-  return result
+    return result as MergeObjectsArray<[First, ...Rest]>
 }
